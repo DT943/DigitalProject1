a33b255883e07b82d67a20b42a055958256efc4f
Tarek Sheikh Alard <tareksh1996job.ts@example.com>
2025-03-13 12:09:50 +0300

    add file type to the Gallery

--- Application/Gallery.Application/GalleryAppService/GalleryAppService.cs
+++ Application/Gallery.Application/GalleryAppService/GalleryAppService.cs
@@ -25,6 +25,48 @@ namespace Gallery.Application.GalleryAppService
             _serviceDbContext = serviceDbContext;
         }
 
+        public override async Task<IEnumerable<GalleryGetDto>> GetAll(SieveModel input)
+        {
+            // Get all galleries first
+            var allGallery = await base.GetAll(input);
+
+            // Get distinct file types for each gallery in a single query and map them directly
+            var fileTypes = await _serviceDbContext.Files
+                .Where(file => allGallery.Select(g => g.Id).Contains(file.GalleryId))
+                .GroupBy(file => file.GalleryId)
+                .Select(group => new
+                {
+                    GalleryId = group.Key,
+                    FileTypes = group.Select(file => file.FileType).Distinct().ToList()
+                })
+                .ToListAsync();
+
+            // Create a dictionary to map galleryId to its file types
+            var fileTypesDictionary = fileTypes.ToDictionary(x => x.GalleryId, x => x.FileTypes);
+
+            // Set the file types for each gallery directly from the dictionary
+            foreach (var item in allGallery)
+            {
+                if (fileTypesDictionary.ContainsKey(item.Id))
+                {
+                    item.FileTypes = fileTypesDictionary[item.Id];
+                }
+            }
+
+            return allGallery;
+        }
+
+
+        public override async Task<GalleryGetDto> Get(int id)
+        {
+
+            var  gallery = await base.Get(id);
+            gallery.FileTypes = await _serviceDbContext.Files.Where(x => x.GalleryId == id).Select(g => g.FileType).Distinct().ToListAsync();
+
+            return gallery;
+        }
+
+
         protected override IQueryable<Domain.Models.Gallery> QueryExcuter(SieveModel input)
         {
             return base.QueryExcuter(input);
