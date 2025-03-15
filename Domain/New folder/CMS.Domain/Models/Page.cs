d4fa4f1f5c59c1518d21b6ecd80f18f22610185e
Tarek Sheikh Alard <tareksh1996job.ts@example.com>
2025-03-15 10:59:28 +0300

    add the status to page - not completed yet

--- Domain/New folder/CMS.Domain/Models/Page.cs
+++ Domain/New folder/CMS.Domain/Models/Page.cs
@@ -14,6 +14,7 @@ namespace CMS.Domain.Models
         public string Language { get; set; }
         public string POS { get; set; }
         public string Title { get; set; }
+        public string Status { get; set; }
         public string Description { get; set; }
         public ICollection<Segment> Segments { get; set; } = new List<Segment>();
 
