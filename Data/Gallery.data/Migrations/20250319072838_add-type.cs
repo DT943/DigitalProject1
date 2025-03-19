0a27af8c47adfda90ab9cb3863e8a05c20ff8ecd
Tarek Sheikh Alard <tareksh1996job.ts@example.com>
2025-03-19 13:35:07 +0300

    add gallery type

---
+++ Data/Gallery.data/Migrations/20250319072838_add-type.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gallery.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addtype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                schema: "GALLERY",
+                table: "Galleries",
+                type: "NVARCHAR2(2000)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                schema: "GALLERY",
+                table: "Galleries");
+        }
+    }
+}
