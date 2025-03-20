241560de42ff02f5cb39eeecbdc9d79111238853
Lubna Al-Halabi <lalhalabi@SAW.LOCAL>
2025-03-20 12:52:40 +0300

    hotel-problem

--- Application/Hotel.Application/HotelAppService/Dtos/HotelInput.cs
+++ Application/Hotel.Application/HotelAppService/Dtos/HotelInput.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Gallery.Application.FileAppservice.Dtos;
 using Hotel.Application.ContactInfoAppService.Dtos;
 using Hotel.Application.HotelGalleryAppService.Dtos;
 using Hotel.Domain.Models;
@@ -17,19 +16,14 @@ namespace Hotel.Application.HotelAppService.Dtos
     {
         public string Name { get; set; }
         public string POS { get; set; }
-        public string Country { get; set; }
         public string Governate { get; set; }
         public string StreetAddress { get; set; }
         public string Url { get; set; }
 
         public int Rank { get; set; }
         public IEnumerable<RoomCreateDto> Rooms { get; set; } = Enumerable.Empty<RoomCreateDto>();
-
-        // public FileCreateDto? CommercialDealsFile { get; set;}
-        // public string? CommercialDealsFileUrlPath { get; set; }
-        // public string? CommercialDealsFileCode { get; set; }
-
         public IEnumerable<HotelGalleryCreateDto> HotelGallery { get; set; } = Enumerable.Empty<HotelGalleryCreateDto>();
+
         public IEnumerable<CotactInfoCreateDto> ContactInfo { get; set; } = Enumerable.Empty<CotactInfoCreateDto>();
         public bool HasAirConditioning { get; set; }
         public bool HasBar { get; set; }
@@ -45,15 +39,15 @@ namespace Hotel.Application.HotelAppService.Dtos
 
     public class HotelCreateDto : IValidatableDto
     {
+
         public string Name { get; set; }
         public string POS { get; set; }
+        public string Country { get; set; }
         public string Governate { get; set; }
         public string StreetAddress { get; set; }
         public string Url { get; set; }
 
         public int Rank { get; set; }
-        public IEnumerable<HotelGalleryCreateDto> HotelGallery { get; set; } = Enumerable.Empty<HotelGalleryCreateDto>();
-
         public IEnumerable<CotactInfoCreateDto> ContactInfo { get; set; } = Enumerable.Empty<CotactInfoCreateDto>();
         public bool HasAirConditioning { get; set; }
         public bool HasBar { get; set; }
@@ -67,12 +61,12 @@ namespace Hotel.Application.HotelAppService.Dtos
 
     }
 
+
     public class HotelUpdateDto : IEntityUpdateDto
     {
 
         public string Name { get; set; }
         public string POS { get; set; }
-        public string Country { get; set; }
         public string Governate { get; set; }
         public string StreetAddress { get; set; }
         public string Url { get; set; }
