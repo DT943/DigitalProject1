e5cbef467cefb59e8708af42e62e11a1773d6163
DT943 <yasseribrahim943@gmail.com>
2025-03-26 11:45:08 +0300

    add-email

--- Application/Authentication.Application/Dtos/AuthenticationOutput.cs
+++ Application/Authentication.Application/Dtos/AuthenticationOutput.cs
@@ -12,8 +12,8 @@ namespace Authentication.Application.Dtos
         public string Code { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string MotherName { get; set; }
-        public string FatherName { get; set; }
+
+        public string Email { get; set; }
 
         public bool IsActive { get; set; }
         [MaxLength(50)]
