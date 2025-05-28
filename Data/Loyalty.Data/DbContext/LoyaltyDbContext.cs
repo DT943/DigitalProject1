e5315b8a5940668d75a5578d760c0a22f35415bd
Tarek Sheikh Alard <tareksh1996job.ts@example.com>
2025-05-28 10:01:02 +0300

    add member tier details

--- Data/Loyalty.Data/DbContext/LoyaltyDbContext.cs
+++ Data/Loyalty.Data/DbContext/LoyaltyDbContext.cs
@@ -35,5 +35,7 @@ namespace Loyalty.Data.DbContext
         public DbSet<Domain.Models.MemberEducationalDetails>     MemberEducationaldetails { get; set; }
         public DbSet<Domain.Models.MemberAccrualTransactions>    MemberAccrualTransactions { get; set; }
         public DbSet<Domain.Models.SegmentMiles> SegmentMiles { get; set; }
+        public DbSet<Domain.Models.MemberTierDetails> MemberTierDetails { get; set; }
+
     }
 }
---
+++ Domain/Loyalty.Domain/Models/MemberTierDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Domain.Models;
+
+namespace Loyalty.Domain.Models
+{
+    public class MemberTierDetails : BasicEntity
+    {
+        [MaxLength(100)]
+        public string CIS { get; set; }
+
+        public int? ReversalId { get; set; }
+
+        public string TierId { get; set; } = "Blue";
+
+        public DateTime? BeginDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public int? UnitTotal { get; set; }
+
+        public int? TXTotal { get; set; }
+
+        public DateTime? FulfillDate { get; set; } = DateTime.Now;
+
+        public string? TierUpgradeType { get; set; }
+
+        public string? Reason { get; set; }
+
+    }
+}
